ff2efb980a9b6190bf066fdac6b9def1084bcefc
cliche818 <jeff.chun.yinli@gmail.com>
2020-03-23 17:27:45 -0400

    create files_storage folder if it does not exist

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -4,5 +4,14 @@ class UploadController < ApplicationController
   def create
     # can confirm sending curl -F 'file=@"invoices/HubdocInvoice1.pdf"' -F 'email=user@domain.com' localhost:3000/upload
     # see file in params[:file] and email in params[:email]
+    file = params[:file]
+
+    unless Dir.exist?(Rails.root.to_s + "/files_storage")
+      FileUtils.mkdir(Rails.root.to_s + "/files_storage")
+    end
+
+    FileUtils.mv(file.tempfile.path, Rails.root.to_s + "/files_storage/#{file.original_filename}")
+
+    render json: {}
   end
 end
